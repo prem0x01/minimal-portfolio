c3a3755cab126538c65a9c531950774a4c58ef70
prem0x01 <mankars130@gmail.com>
2025-06-08 12:08:36 +0530

    Solved Version conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    server: {
-        allowedHosts: [
-            'd462-2401-4900-8fca-ffb2-b875-f7b6-9a39-6362.ngrok-free.app'
-        ]
-    }
+    plugins: [sveltekit()]
 });
